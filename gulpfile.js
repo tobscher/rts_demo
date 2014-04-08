0573ba6865e2db5f8bebb6fe807bb24dbc82cac9
Tobias Haar <tobias.haar@googlemail.com>
2014-04-08 22:55:25 +0100

    updated gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,32 @@
 var gulp = require('gulp');
+
 var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
-var watch = require('gulp-watch');
 var concat = require('gulp-concat');
 
 var paths = {
-  scripts: ['js/**/*.js'],
-  stylesheets: 'scss//**/*'
+  scripts: './js/*.js',
+  stylesheets: './scss/*.scss'
 };
 
 gulp.task('sass', function () {
-  gulp.src('./scss/*.scss')
+  return gulp.src(paths.stylesheets)
     .pipe(sass())
     .pipe(concat('application.css'))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('scripts', function() {
- return gulp.src(paths.scripts)
+ gulp.src(paths.scripts)
     .pipe(concat('application.js'))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('scripts.min', function() {
  return gulp.src(paths.scripts)
     .pipe(uglify())
     .pipe(concat('application.min.js'))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('watch', function() {
