888245470627ff1ba421e53da20e170db7275deb
Tobias Haar <tobias.haar@googlemail.com>
2014-08-13 23:33:21 +0100

    scv takes 17s to build

--- js/rts/commands/build_scv.js
+++ js/rts/commands/build_scv.js
@@ -2,7 +2,7 @@ RTS.Commands.BuildSCV = function(commandCentre) {
   this.action = "build SCV";
   this.message = "Press 'S' to <span class='action'>" + this.action + "</span>";
   this.icon = "img/build-scv.svg";
-  this.duration = 5000;
+  this.duration = 17000;
   this.span = false;
   this.commandCentre = commandCentre;
 };
