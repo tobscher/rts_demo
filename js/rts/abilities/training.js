4a5c1c2a3edbf2cdef961ce2deedf3cc650d0a35
Tobias Haar <tobias.haar@gmail.com>
2014-08-14 13:01:09 +0100

    added array to queue builds

--- js/rts/abilities/training.js
+++ js/rts/abilities/training.js
@@ -19,8 +19,13 @@ RTS.Abilities.TrainingScript = function(options) {
   options = options || {};
   RTS.Abilities.Base.call(this, options);
 
+  this.limit = 5;
+  this.builds = [];
+  this.current = null;
   this.building = false;
   this.clock = new THREE.Clock();
+
+  this.hud = RTS.HUD.instance;
 };
 
 inherits(RTS.Abilities.TrainingScript, RTS.Abilities.Base);
@@ -29,27 +34,47 @@ RTS.Abilities.TrainingScript.prototype.realize = function() {
 }
 
 RTS.Abilities.TrainingScript.prototype.update = function() {
+  // Get next build from queue
+  if (this.current == null) {
+    if (this.builds.length > 0) {
+      this.next(this.builds[0]);
+    }
+  }
+
   if (!this.building) return;
 
-  var hud = RTS.HUD.instance;
-
   var elapsed = this.clock.getElapsedTime() * 1000.0;
-  var percentCompleted = (elapsed / this.build.duration) * 100
-  hud.buildQueue.update(percentCompleted);
+  var percentCompleted = (elapsed / this.current.duration) * 100
+  this.hud.buildQueue.update(percentCompleted);
 
 
-  if (elapsed >= this.build.duration) {
-    // Finished
-    hud.buildQueue.update(0);
-    this.build = null;
-    this.building = false;
-    this.clock.elapsedTime = 0.0;
-    this.clock.stop();
+  if (elapsed >= this.current.duration) {
+    this.finish();
   }
 };
 
 RTS.Abilities.TrainingScript.prototype.push = function(build) {
-  this.build = build;
+  if (this.builds.length == this.limit) {
+    // Queue is full
+    return;
+  }
+
+  this.builds.push(build);
+};
+
+RTS.Abilities.TrainingScript.prototype.finish = function() {
+  this.builds.shift();
+  this.hud.buildQueue.update(0);
+
+  this.current = null;
+  this.building = false;
+
+  this.clock.stop();
+  this.clock.elapsedTime = 0.0;
+};
+
+RTS.Abilities.TrainingScript.prototype.next = function(build) {
+  this.current = build;
   this.building = true;
   this.clock.start();
 };
